e2f2fa7fb10bb84912b5e84b5d8474f379240954
Lucas Melin <lucas.melin@gmail.com>
2021-08-27 18:12:15 -0400

    :tada: Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Cookiecutter Python Project
+
+[Cookiecutter](https://github.com/cookiecutter/cookiecutter) template for a Python project.
---
+++ {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
@@ -0,0 +1,4 @@
+"""Tests for `{{ cookiecutter.project_slug }}` package."""
+import pytest
+
+from {{ cookiecutter.project_slug }} import {{ cookiecutter.project_slug }}
