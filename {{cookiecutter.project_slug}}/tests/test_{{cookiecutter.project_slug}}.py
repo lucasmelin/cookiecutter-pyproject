f53d387d00019437a283bbb7f56894c4a7ddd849
Lucas Melin <lucasmelin@users.noreply.github.com>
2021-09-14 08:25:16 -0400

    Update test_{{cookiecutter.project_slug}}.py

--- {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
@@ -1,4 +1,7 @@
 """Tests for `{{ cookiecutter.project_slug }}` package."""
 import pytest
+import {{ cookiecutter.project_slug }}
 
-from {{ cookiecutter.project_slug }} import {{ cookiecutter.project_slug }}
+def test_version():
+    """Verify the package version."""
+    assert {{ cookiecutter.project_slug }}.__version__ == {{ cookiecutter.version }}
